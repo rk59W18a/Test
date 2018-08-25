023a8494a95bee9b86d6abab370c495cf80d780c
rk59W18a <42214911+rk59W18a@users.noreply.github.com>
2018-08-25 18:00:04 +0200

    Add files via upload

--- AnagraficaFruitoriTest.java
+++ AnagraficaFruitoriTest.java
@@ -2,26 +2,26 @@ package test_2.punto4;
 
 import static org.junit.Assert.*;
 
-
-
-
 import java.time.LocalDate;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import logica.parte2.punto4.AnagraficaFruitori;
+import logica.parte2.punto4.ArchivioStorico;
 import logica.parte2.punto4.Fruitore;
 
 public class AnagraficaFruitoriTest {
 
 	private AnagraficaFruitori af;
+	private ArchivioStorico as;
     private Fruitore f1;
     private Fruitore f2;
 	
 	@Before
     public void initialize() {
 		af = new AnagraficaFruitori();
+		as = new ArchivioStorico();
 		f1 = new Fruitore("Luca", "Rossi", 1995, 8, 25, "lc9", "000");
 		f2 = new Fruitore("Laura", "Bianchi", 1985, 5, 10, "lau", "bi5");
 		
@@ -38,17 +38,17 @@ public class AnagraficaFruitoriTest {
 	}
 	
 	@Test
-	public void fruitoreGiàPresente() {	
+	public void fruitoreGiaPresente() {	
 		assertTrue(af.verificaPresenza("Luca", "Rossi", LocalDate.of(1995, 8, 25)));	
 	}
 	
 	@Test
-	public void fruitoreOmonimoMaEtàDiversaNonPresente() {	
+	public void fruitoreOmonimoMaEtaDiversaNonPresente() {	
 		assertFalse(af.verificaPresenza("Matteo", "Ferrari", LocalDate.of(1985, 4, 20)));	
 	}
 	
 	@Test
-	public void usernameGiàPresente() {
+	public void usernameGiaPresente() {
 		assertTrue(af.verificaStessoUsername(f1.getUsername()));
 	}
 	
@@ -65,6 +65,54 @@ public class AnagraficaFruitoriTest {
 	@Test
 	public void accediSenzaSuccesso() {
 		assertFalse(af.accedi(f2.getUsername(), "op3"));
+	}
+	
+	@Test
+	public void controlloDecadenzaFruitoreSuccessivoAllaDataDiScadenza() {
+		f1.setDataDiScadenza(LocalDate.now().minusDays(1));
+		af.decadenzaFruitore(as);
+		
+		assertFalse(af.getElenco().contains(f1));
+	}
+	
+	@Test
+	public void controlloDecadenzaFruitoreSuccessivoAllaDataDiScadenzaPerArchivioStorico() {
+		f1.setDataDiScadenza(LocalDate.now().minusDays(1));
+		af.decadenzaFruitore(as);
+		
+		assertTrue(as.getDecadenzeFruitoriStoriche().getElenco().contains(f1));
+	}
+	
+	@Test
+	public void controlloDecadenzaFruitoreCoincidenteConLaDataDiScadenza() {
+		f2.setDataDiScadenza(LocalDate.now());
+		af.decadenzaFruitore(as);
+		
+		assertFalse(af.getElenco().contains(f2));
+	}
+	
+	@Test
+	public void controlloDecadenzaFruitoreCoincidenteConLaDataDiScadenzaPerArchivioStorico() {
+		f2.setDataDiScadenza(LocalDate.now().minusDays(1));
+		af.decadenzaFruitore(as);
+		
+		assertTrue(as.getDecadenzeFruitoriStoriche().getElenco().contains(f2));
+	}
+	
+	@Test
+	public void controlloDecadenzaFruitorePrecedenteAllaDataDiScadenzaPerArchivioStorico() {
+		f1.setDataDiScadenza(LocalDate.now().plusDays(1));
+		af.decadenzaFruitore(as);
+		
+		assertFalse(as.getDecadenzeFruitoriStoriche().getElenco().contains(f1));
+	}
+	
+	@Test
+	public void controlloDecadenzaFruitorePrecedenteAllaDataDiScadenza() {
+		f1.setDataDiScadenza(LocalDate.now().plusDays(1));
+		af.decadenzaFruitore(as);
+		
+		assertTrue(af.getElenco().contains(f1));
 	}
 
 }
