12a0f0d5725bcfe6258a0e043356012aca1d309a
rk59W18a <42214911+rk59W18a@users.noreply.github.com>
2018-08-25 17:59:36 +0200

    Add files via upload

--- AnagraficaFruitoriTest.java
+++ AnagraficaFruitoriTest.java
@@ -2,6 +2,9 @@ package test_2.punto3;
 
 import static org.junit.Assert.*;
 
+
+
+
 import java.time.LocalDate;
 
 import org.junit.Before;
@@ -13,10 +16,10 @@ import logica.parte2.punto3.Fruitore;
 
 public class AnagraficaFruitoriTest {
 
-    private AnagraficaFruitori af;
-    private ArchivioStorico as;
-	private Fruitore f1;
-	private Fruitore f2;
+	private AnagraficaFruitori af;
+	private ArchivioStorico as;
+    private Fruitore f1;
+    private Fruitore f2;
 	
 	@Before
     public void initialize() {
@@ -24,6 +27,7 @@ public class AnagraficaFruitoriTest {
 		as = new ArchivioStorico();
 		f1 = new Fruitore("Luca", "Rossi", 1995, 8, 25, "lc9", "000");
 		f2 = new Fruitore("Laura", "Bianchi", 1985, 5, 10, "lau", "bi5");
+		
 		af.aggiungiFruitore(f1);
 		af.aggiungiFruitore(f2);
 	}
@@ -68,55 +72,50 @@ public class AnagraficaFruitoriTest {
 	
 	@Test
 	public void controlloDecadenzaFruitoreSuccessivoAllaDataDiScadenza() {
-		
-		fru1.setDataDiScadenza(LocalDate.now().minusDays(1));
+		f1.setDataDiScadenza(LocalDate.now().minusDays(1));
 		af.decadenzaFruitore(as);
 		
-		assertFalse(af.getElenco().contains(fru1));
+		assertFalse(af.getElenco().contains(f1));
 	}
 	
 	@Test
 	public void controlloDecadenzaFruitoreSuccessivoAllaDataDiScadenzaPerArchivioStorico() {
-		
-		fru1.setDataDiScadenza(LocalDate.now().minusDays(1));
+		f1.setDataDiScadenza(LocalDate.now().minusDays(1));
 		af.decadenzaFruitore(as);
 		
-		assertTrue(as.getDecadenzeFruitoriStoriche().getElenco().contains(fru1));
+		assertTrue(as.getDecadenzeFruitoriStoriche().getElenco().contains(f1));
 	}
 	
 	@Test
 	public void controlloDecadenzaFruitoreCoincidenteConLaDataDiScadenza() {
-		
-		fru2.setDataDiScadenza(LocalDate.now());
+		f2.setDataDiScadenza(LocalDate.now());
 		af.decadenzaFruitore(as);
 		
-		assertFalse(af.getElenco().contains(fru2));
+		assertFalse(af.getElenco().contains(f2));
 	}
 	
 	@Test
 	public void controlloDecadenzaFruitoreCoincidenteConLaDataDiScadenzaPerArchivioStorico() {
-		
-		fru2.setDataDiScadenza(LocalDate.now().minusDays(1));
+		f2.setDataDiScadenza(LocalDate.now().minusDays(1));
 		af.decadenzaFruitore(as);
 		
-		assertTrue(as.getDecadenzeFruitoriStoriche().getElenco().contains(fru2));
+		assertTrue(as.getDecadenzeFruitoriStoriche().getElenco().contains(f2));
 	}
 	
 	@Test
 	public void controlloDecadenzaFruitorePrecedenteAllaDataDiScadenzaPerArchivioStorico() {
-		
-		fru1.setDataDiScadenza(LocalDate.now().plusDays(1));
+		f1.setDataDiScadenza(LocalDate.now().plusDays(1));
 		af.decadenzaFruitore(as);
 		
-		assertFalse(as.getDecadenzeFruitoriStoriche().getElenco().contains(fru1));
+		assertFalse(as.getDecadenzeFruitoriStoriche().getElenco().contains(f1));
 	}
 	
 	@Test
 	public void controlloDecadenzaFruitorePrecedenteAllaDataDiScadenza() {
-		
-		fru1.setDataDiScadenza(LocalDate.now().plusDays(1));
+		f1.setDataDiScadenza(LocalDate.now().plusDays(1));
 		af.decadenzaFruitore(as);
 		
-		assertTrue(af.getElenco().contains(fru1));
+		assertTrue(af.getElenco().contains(f1));
 	}
+
 }
